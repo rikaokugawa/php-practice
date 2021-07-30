6877688c657054d1fe8cac8bc89d058360bbccf7
奥川利佳 <rinmemory107@gmail.com>
2021-07-30 02:02:28 +0000

    php03課題

---
+++ practice2.php
@@ -0,0 +1,39 @@
+<?php
+if ($name = '奥川利佳'){
+    echo "私はあなたの名前です。";
+  } else {
+    echo "あなたの名前ではありません。";
+  }
+  echo"\n";
+  
+  
+    
+  for($i = 1; $i <= 10000; $i++ ) {
+  $total += $i;
+  }
+    echo $total;
+  echo"\n";
+  
+  
+  $fruits = array("orange", "strawberry", "banna", "papaya", "watermelon");
+  foreach($fruits as $fruit) {
+    echo "要素は" . $fruit;
+    echo "\n";
+  }
+    
+    
+  for($i = 1; $i <= 100;){
+  if($i % 5 ==0)
+  echo $i;
+  }
+  echo"\n"
+  
+  
+
+  
+  
+  
+  
+  
+  
+
