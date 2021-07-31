8a0a10d9498a522d576b0001a9bd77f7c031d08b
奥川利佳 <rinmemory107@gmail.com>
2021-07-31 02:42:58 +0000

    課題13

--- practice1.php
+++ practice1.php
@@ -1,6 +1,42 @@
 <?php
-$a = 3;
-$b = 7;
-echo $a + $b
 
+function sum($max){
+    $result = 0;
+    for ($i = 1; $i <= $max; $i ++){
+        $result += $i;
+        
+}
+return $result;
+}
+echo sum(100);
+echo"\n";
+
+$string = "ABCDEF";
+echo strlen($string);
+echo"\n";
+
+$string = "I love Ruby!";
+$new_string = str_replace("Ruby","PHP", $string );
+echo $new_string;
+echo "\n";
+
+$array = array(1,2,3,4,5,6,7,8,9,10);
+echo count($array);
+echo "\n";
+
+$array = array(2,5,9,7,3,1,8,6,4);
+asort($array);
+print_r($array);
+arsort($array);
+print_r($array);
+echo "\n";
+?>
+
+<?php
+function multiple($i){
+    $result = $i*2;
+}
+return $result;
+echo multiple(5);
+echo"\n";
 
